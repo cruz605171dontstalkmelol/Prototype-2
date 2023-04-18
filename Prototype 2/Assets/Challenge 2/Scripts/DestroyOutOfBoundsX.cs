0061837817286f2b3dddd32f7866eb610754b0e4
cruz605171dontstalkmelol <130406773+cruz605171dontstalkmelol@users.noreply.github.com>
2023-04-18 13:41:17 -0700

    Play Fetch?

---
+++ Prototype 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -44;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy balls if y position is less than bottomLimit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.y < bottomLimit) {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private PlayerInput _playerInput;
+
+    private void Awake() {
+        _playerInput = new PlayerInput();
+    }
+
+    public void OnDogSpawn (InputAction.CallbackContext context) {
+        if (context.started) {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
